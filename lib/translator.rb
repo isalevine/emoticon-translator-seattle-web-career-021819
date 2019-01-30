dba2dc21f3fb8b8afafb438c889336fb2a04f60a
Isa Levine <isaacdlevine@gmail.com>
2019-01-30 06:07:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(filepath)
   library_hash.each do |emoticon_meanings, emoticon_array|
     # emoticon_array[0] is the ENGLISH emoticon, i.e. "O:)"
     # emoticon_array[1] is the JAPANESE emoticon, i.e. "☜(⌒▽⌒)☞"
-    hash_output["get_emoticon"][emoticon_array[0]] = emoticon_meanings
+    hash_output["get_emoticon"][emoticon_array[0]] = emoticon_array[1]
     hash_output["get_meaning"][emoticon_array[1]] = emoticon_meanings
   end
 
